385567a2e889beea726b13a4153e59eb2c25c735
Joakim Karlsson <jk@patientsky.com>
2017-03-27 16:24:03 +0200

    updated build scripts

--- build.sh
+++ build.sh
@@ -4,5 +4,3 @@ GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ./bin/k8s-sslmate .
 cd bin
 zip k8s-sslmate.zip k8s-sslmate
 scp -4 k8s-sslmate.zip roffe@roffe.nu:/webroot/roffe/public_html/k8s-sslmate/
-#git commit -am "Binary build"
-#git push
--- buildDocker.sh
+++ buildDocker.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 docker build -t roffe/k8s-sslmate .
-# && docker run --rm -it --name k8s-sslmate -e SSLMATE_API_KEY="abcde" -e SSLMATE_CHECKTIME=60 roffe/k8s-sslmate
